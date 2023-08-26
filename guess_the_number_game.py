47214bef8ac7ee84be928175f3a1006482dd8056
PoojaBorad <poojabborad@gmail.com>
2023-08-26 16:17:05 -0700

    Update guess_the_number_game.py

--- guess_the_number_game.py
+++ guess_the_number_game.py
@@ -5,7 +5,6 @@ secret_number = random.randint(1,50)
 print("Welcome to the Guess the Number Game!")
 print("Here I am guessing number from 1 to 50. can you guess the number?")
 
-guess_number = 5
 
 while True:
     guess = int(input("Enter your guessing number: "))
@@ -16,4 +15,9 @@ while True:
     elif guess > secret_number:
         print("Your guess is high, please guess lower number.")
     else:
-        print("Your guess is low, please huess high number. ")    
+        print("Your guess is low, please guess high number. ")       
+
+
+    
+        
+
